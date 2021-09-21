d821620257788f2ca11d48b96dbabf7c51c1b057
jurasach <jurasach@mail.ru>
2021-09-21 19:07:40 +0300

    тесты

--- myfinishproject/bboard/tests/test_models.py
+++ myfinishproject/bboard/tests/test_models.py
@@ -1,19 +1,2 @@
 from django.test import TestCase
 from myfinishproject.bboard import models
-
-
-class BbModelsTest(TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        Bb.object.create(
-            title='Bike',
-            content='good look',
-            price='200',
-            phone='+375000000000'
-        )
-
-    def test_title_label(self):
-        title_example = Bb.object.get(id=1)
-        field_label = title_example._meta.get.field('title').verbose.name
-        self.assertEqual(field_label, 'title')
